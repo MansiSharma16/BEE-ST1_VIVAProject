26fbe8d221140403b69f2927872a516639ad1b65
MansiSharma16 <104722253+MansiSharma16@users.noreply.github.com>
2023-12-01 16:00:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const db=require("./middlewire/db")
 const router=require("./routes/router")
 
 app.use(express.json());
-db.dbConnect();
+db.Connect();
 app.use("/api",router);
 app.listen(3000,()=>{
     console.log("server on port 3000");
